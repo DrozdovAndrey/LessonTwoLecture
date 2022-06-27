cd458d2322a09a424add48dc1e66010d628a95bc
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-27 14:28:28 +0300

    Exampes011 used void Fill and print array

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection) // функция заполнения массива
+    {
+        int length = collection.Length;
+        int index = 0;
+        while (index < length)
+            {
+                collection[index] = new Random().Next(1, 10);  // рендомными значениями от 1 до 9
+                index++;
+            }
+    }
+
+void PrintArray(int[] col)  // void невозвратный метод    // выводит массив 
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+            {
+                Console.WriteLine(col[position]);
+                position++;
+            }
+    }
+
+int[] array = new int[10]; // new int[] создает новый массив с "0", 10 - коллличество элементов 
+
+FillArray(array);
+PrintArray(array);
+
