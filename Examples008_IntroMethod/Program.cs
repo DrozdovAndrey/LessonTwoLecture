b03372161c1b9c6c38be528b2b39de36c69c6ac2
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-27 11:17:49 +0300

    Exampes008 used function Max

--- Examples008_IntroMethod/Program.cs
+++ Examples008_IntroMethod/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12225;  
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 1223;
+int c2 = 333333;
+int a3 = 13;
+int b3 = 23;
+int c3 = 313;
+
+// int max1 = Max(a1, b1, c1);      //исполльзовали функцию Max - ище большее из 3х
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+// int max = Max(       // можно записывать перенося строки
+//     Max(a1, b1, c1),   // использовали функциональное пронраммирование
+//     Max(a2, b2, c2), 
+//     Max(a3, b3, c3));
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));   // можно использовать так
+
+// int max = a1;
+// if (b1 > max) max = b1;      // в этом приеере мы написали код в обычном виде
+// if (c1 > max) max = c1;
+
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
+
+Console.WriteLine(max);
