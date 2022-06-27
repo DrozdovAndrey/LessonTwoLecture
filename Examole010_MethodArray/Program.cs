90ebe450e2ea1adbc91fbd02a697e68cb736f4ea
Andrey Drozdov <drozdov_andrey@list.ru>
2022-06-27 13:54:54 +0300

    Exampes010 used break

--- Examole010_MethodArray/Program.cs
+++ Examole010_MethodArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// надо погуглить как переименовать папки в гите
+
+int [] array = {1,12,32,4,18,15,16,71,18};
+
+int n = array.Length; // длинна массива, или колличество элементов массива
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;  // завершение условия
+    }
+    index++;
+}
